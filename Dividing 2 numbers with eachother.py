a051071ef48efb618162dd788f791686a0c0a683
Adam OTP <145787687+AceAdxm@users.noreply.github.com>
2025-10-25 23:50:46 +0100

    Add script to divide two numbers

---
+++ Dividing 2 numbers with eachother.py
@@ -0,0 +1,9 @@
+# Obtain 2 numbers by declaring the variables
+num1 = float(input("Please enter the first number: "))
+num2 = float(input("Please enter the second number: "))
+
+# Divide num1 by num2 
+result = num1 / num2 
+
+# Show the output
+print("Result:", result)
