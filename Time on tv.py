5ac8256bf4ef39909bdb2a8cee9c98f0b681642c
Adam OTP <145787687+AceAdxm@users.noreply.github.com>
2025-10-26 00:23:10 +0100

    Add TV watching time feedback script

---
+++ Time on tv.py
@@ -0,0 +1,8 @@
+time = float(input("How Many hours do you spend watching tv each day"))
+
+if time < 2:
+    print("That shouldnt rot your brain!")
+elif time < 4:
+    print("Arent you getting square eyes?")
+else:
+    print("Fresh air is better than flicking through channels")
