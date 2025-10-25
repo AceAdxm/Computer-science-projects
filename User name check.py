22210f4544359fd32f52936a305d354fc55af7cb
Adam OTP <145787687+AceAdxm@users.noreply.github.com>
2025-10-26 00:11:57 +0100

    Add user name check functionality

---
+++ User name check.py
@@ -0,0 +1,10 @@
+#Asks the user their name from a declared variable ---> input
+name = input("What is your name? ")
+
+# Checks if the users name is `Adam`
+if name.lower() == "adam":
+    print("You're cool")
+
+#Other alternative
+else:
+    print("Nice to meet you")
